6f120ba4d1c4936f7d0e96f8496cadc9ffc0d6e8
Glitch (atolling-sovereignty) <none>
2021-04-08 01:15:01 +0000

    First Try

---
+++ voices.js
@@ -0,0 +1,39 @@
+// This isn't necessary but it keeps the editor from thinking L and carto are typos
+/* global L, carto */
+
+var map = L.map('map', {
+  center: [7, 170],
+  zoom: 4
+});
+
+// Add base layer
+L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager_nolabels/{z}/{x}/{y}.png', {
+  maxZoom: 18
+}).addTo(map);
+
+// Initialize Carto
+var client = new carto.Client({
+  apiKey: 'default_public',
+  username: 'chuc873'
+});
+
+
+
+//Initialze data source
+var source = new carto.source.SQL("SELECT * FROM marshallese_voices");
+
+// Create style for the data
+var style = new carto.style.CartoCSS(`
+  #layer {
+    polygon-fill: red;
+  }
+`);
+
+
+
+// Add style to the data
+var layer = new carto.layer.Layer(source, style);
+
+// Add the data to the map as a layer
+client.addLayer(layer);
+client.getLeafletLayer().addTo(map);
